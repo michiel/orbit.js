966963829fc019a19de20e12eb6fff75ac4bcf7c
Michiel Kalkman <michiel@michielkalkman.com>
2014-02-01 00:50:08 +0100

    Add indexed_db_source and test with stub methods

---
+++ test/tests/orbit_common/unit/indexed_db_source_test.js
@@ -0,0 +1,177 @@
+import Orbit from 'orbit/main';
+import IndexedDBSource from 'orbit_common/indexed_db_source';
+import { all, Promise } from 'rsvp';
+
+var source;
+
+//
+// Helper methods
+//
+
+function verifyIndexedDBIsEmpty(namespace) {
+  return false;
+}
+
+function verifyIndexedDBContainsRecord(namespace, type, record, ignoreFields) {
+  return false;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+module("OrbitCoreSources - IndexedDBSource", {
+  setup: function() {
+    Orbit.Promise = Promise;
+
+    var schema = {
+      idField: '__id',
+      models: {
+        planet: {
+          attributes: {
+            name: {type: 'string'},
+            classification: {type: 'string'}
+          },
+          links: {
+            moons: {type: 'hasMany', model: 'moon', inverse: 'planet'}
+          }
+        },
+        moon: {
+          attributes: {
+            name: {type: 'string'}
+          },
+          links: {
+            planet: {type: 'hasOne', model: 'planet', inverse: 'moons'}
+          }
+        }
+      }
+    };
+
+    source = new IndexedDBSource(schema, {
+        namespace : 'planets',
+        version   : 1
+      });
+  },
+
+  teardown: function() {
+    window.indexedDB.deleteDatabase(source.namespace);
+    source = null;
+    Orbit.Promise = null;
+  }
+});
+
+test("it exists", function() {
+  ok(source);
+});
+
+test("#add - can insert records and assign ids", function() {
+  expect(4);
+
+  source.add('planet', {name: 'Jupiter', classification: 'gas giant'}).then(function(planet) {
+    ok(planet.__id, 'orbit id should be defined');
+    equal(planet.id, 12345, 'server id should be defined');
+    equal(planet.name, 'Jupiter', 'name should match');
+    equal(planet.classification, 'gas giant', 'classification should match');
+  });
+});
+
+test("#update - can update records", function() {
+  expect(4);
+
+  source.update('planet', {id: 12345, name: 'Jupiter', classification: 'gas giant'}).then(function(planet) {
+    ok(planet.__id, 'orbit id should be defined');
+    equal(planet.id, 12345, 'server id should be defined');
+    equal(planet.name, 'Jupiter', 'name should match');
+    equal(planet.classification, 'gas giant', 'classification should match');
+  });
+});
+
+test("#patch - can patch records", function() {
+  expect(1);
+
+  source.patch('planet', {id: 12345}, 'classification', 'gas giant').then(function() {
+    ok(true, 'record patched');
+  });
+});
+
+test("#remove - can delete records", function() {
+  expect(1);
+
+  source.remove('planet', {id: 12345}).then(function() {
+    ok(true, 'record deleted');
+  });
+});
+
+test("#link - can patch records with inverse relationships", function() {
+  expect(1);
+
+  source.link('planet', {id: 12345}, 'moons', {id: 987}).then(function() {
+    ok(true, 'records linked');
+  });
+});
+
+test("#unlink - can patch records with inverse relationships", function() {
+  expect(1);
+
+  source.unlink('planet', {id: 12345}, 'moons', {id: 987}).then(function() {
+    ok(true, 'records unlinked');
+  });
+});
+
+test("#find - can find individual records by passing in a single id", function() {
+  expect(5);
+
+  source.add('planet', {name: 'Jupiter', classification: 'gas giant'}).then(function(planet) {
+    source.find('planet', planet.__id).then(function(planet) {
+      ok(planet.__id, 'orbit id should be defined');
+      equal(planet.id, 12345, 'server id should be defined');
+      equal(planet.name, 'Jupiter', 'name should match');
+      equal(planet.classification, 'gas giant', 'classification should match');
+    });
+  });
+});
+
+test("#find - can find all records", function() {
+  expect(13);
+
+  var records = [
+    {id: 1, name: 'Jupiter', classification: 'gas giant'},
+    {id: 2, name: 'Earth', classification: 'terrestrial'},
+    {id: 3, name: 'Saturn', classification: 'gas giant'}
+  ];
+
+  source.find('planet').then(function(planets) {
+
+    var planet, record;
+    for (var i = 0; i < planets.length; i++) {
+      planet = planets[i];
+      record = records[i];
+      ok(planet.__id, 'orbit id should be defined');
+      equal(planet.id, record.id, 'server id should be defined');
+      equal(planet.name, record.name, 'name should match');
+      equal(planet.classification, record.classification, 'classification should match');
+    }
+  });
+});
+
+test("#find - can filter records", function() {
+  expect(18);
+
+  var records = [
+    {id: 1, name: 'Mercury', classification: 'terrestrial'},
+    {id: 2, name: 'Venus', classification: 'terrestrial'},
+    {id: 3, name: 'Earth', classification: 'terrestrial'},
+    {id: 4, name: 'Mars', classification: 'terrestrial'}
+  ];
+
+  source.find('planet', {classification: 'terrestrial'}).then(function(planets) {
+
+    var planet, record;
+    for (var i = 0; i < planets.length; i++) {
+      planet = planets[i];
+      record = records[i];
+      ok(planet.__id, 'orbit id should be defined');
+      equal(planet.id, record.id, 'server id should be defined');
+      equal(planet.name, record.name, 'name should match');
+      equal(planet.classification, record.classification, 'classification should match');
+    }
+  });
+});
